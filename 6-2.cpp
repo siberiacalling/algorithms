0316820a67fd1701ecfcfcc47c19e174550c0c2d
Anita <smirnovasiberia@gmail.com>
2019-03-23 01:07:47 +0300

    6-2 int_64

--- 6-2.cpp
+++ 6-2.cpp
@@ -8,13 +8,13 @@
 #include <algorithm>
 #include <iostream>
 
-int medianThree(const int *arr, int left, int right) {
-    int a = arr[right];
-    int b = arr[left];
-    int c = arr[(right - left) / 2 + left];
-    int max = std::max(std::max(a, b), c);
-    int min = std::min(std::min(a, b), c);
-    int median = a + b + c - (min + max);
+int64_t medianThree(const int64_t *arr, int64_t left, int64_t right) {
+    int64_t a = arr[right];
+    int64_t b = arr[left];
+    int64_t c = arr[(right - left) / 2 + left];
+    int64_t max = std::max(std::max(a, b), c);
+    int64_t min = std::min(std::min(a, b), c);
+    int64_t median = a + b + c - (min + max);
     if (median == a) {
         return right;
     }
@@ -24,20 +24,20 @@ int medianThree(const int *arr, int left, int right) {
     return (right - left) / 2 + left;
 }
 
-void print_arr(int *arr, int left, int right) {
-    for (int i = left; i < right; i++) {
+void print64_t_arr(int64_t *arr, int64_t left, int64_t right) {
+    for (int64_t i = left; i < right; i++) {
         std::cout << arr[i] << " ";
     }
     std::cout << '\n';
 }
 
 
-int partition(int *arr, int left, int right) {
-    int pivot_index = medianThree(arr, left, right);
-    int pivot = arr[pivot_index];
+int64_t partition(int64_t *arr, int64_t left, int64_t right) {
+    int64_t pivot_index = medianThree(arr, left, right);
+    int64_t pivot = arr[pivot_index];
     std::swap(arr[right], arr[pivot_index]);
-    int i = left;
-    int j = left;
+    int64_t i = left;
+    int64_t j = left;
     while (j != right) {
         if (arr[j] > pivot) {
             j++;
@@ -51,11 +51,11 @@ int partition(int *arr, int left, int right) {
     return i;
 }
 
-int findKStat(int *arr, int array_length, int k) {
-    int left = 0;
-    int right = array_length - 1;
+int64_t findKStat(int64_t *arr, int64_t array_length, int64_t k) {
+    int64_t left = 0;
+    int64_t right = array_length - 1;
     while (true) {
-        int pivotPos = partition(arr, left, right);
+        int64_t pivotPos = partition(arr, left, right);
         if (k == pivotPos) {
             return arr[k];
         }
@@ -68,13 +68,14 @@ int findKStat(int *arr, int array_length, int k) {
 }
 
 int main() {
-    int array_length = 0;
-    int k = 0;
+    int64_t array_length = 0;
+    int64_t k = 0;
     std::cin >> array_length >> k;
-    int *arr = new int[array_length];
-    for (int i = 0; i < array_length; i++) {
+    int64_t *arr = new int64_t[array_length];
+    for (int64_t i = 0; i < array_length; i++) {
         std::cin >> arr[i];
     }
     std::cout << findKStat(arr, array_length, k);
     delete [] arr;
+    return 0;
 }
